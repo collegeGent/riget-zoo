a8961e18642786573caa57018a0b77b84fdc3108
collegeGent <rgent@rnngroup.ac.uk>
2024-11-18 16:50:37 +0000

    Created Nav area

--- dcrm/rigetZoo/views.py
+++ dcrm/rigetZoo/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/base.html')
+    context={'bgImage': 'tiger.jpg',}
+    return render(request, 'pages/index.html', context=context)
