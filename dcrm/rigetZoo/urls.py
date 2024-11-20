f8ae22eaeea3837e3558d94e76d61845c7539854
collegeGent <rgent@rnngroup.ac.uk>
2024-11-20 16:41:50 +0000

    Created more pages

--- dcrm/rigetZoo/urls.py
+++ dcrm/rigetZoo/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.home, name=''),
-    path('example', views.example, name='example')
+    path('visitUs', views.visitUs, name='visitUs'),
+    path('visitEdu', views.visitEdu, name='visitEdu'),
+    path('whatsHere', views.whatsHere, name='whatsHere'),
+    path('aboutUs', views.aboutUs, name='aboutUs'),
+    path('example', views.example, name='example'),
 
 ]
--- dcrm/rigetZoo/views.py
+++ dcrm/rigetZoo/views.py
@@ -6,6 +6,25 @@ def home(request):
     context={'bgImage': 'tiger.jpg',}
     return render(request, webpage, context=context)
 
+def visitUs(request):
+    webpage = 'pages/visitUs.html'
+    context={'bgImage': 'parrot.jpg',}
+    return render(request, webpage, context=context)
+
+def visitEdu(request):
+    webpage = 'pages/visitEdu.html'
+    context={'bgImage': 'schoolVisit.jpg',}
+    return render(request, webpage, context=context)
+
+def whatsHere(request):
+    webpage = 'pages/whatsHere.html'
+    context={'bgImage': 'parkEntrance.jpg',}
+    return render(request, webpage, context=context)
+
+def aboutUs(request):
+    webpage = 'pages/aboutUs.html'
+    context={'bgImage': 'teamphoto.jpg',}
+    return render(request, webpage, context=context)
 
 def example(request):
 
